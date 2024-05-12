3d73737d17798e415d273f21a632a1f8f61b8ef3
Mozaffar <FERAWWWESS@gmail.com>
2024-05-12 16:35:28 +0300

    edit-and-delete-message

--- routes/messages.js
+++ routes/messages.js
@@ -43,9 +43,52 @@ async function sendMessage(req, res) {
   }
 }
 
+/* Edit message */
+async function editMessage(req, res) {
+  const messageID = req.params.messageID;
+  const { message, timestamp } = req.body;
+  try {
+    const updatedMessage = await MessageBox.findOneAndUpdate(
+      { _id: messageID },
+      { message: message, timestamp: timestamp },
+      { new: true }
+    );
+
+    if (updatedMessage) {
+      res.send("Update message success");
+    } else {
+      res.status(404).json({ error: "Message not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+/* Delete message */
+async function deleteMessage(req, res) {
+  const messageID = req.params.messageID;
+  try {
+    const updatedMessage = await MessageBox.findOneAndDelete({
+      _id: messageID,
+    });
+
+    if (updatedMessage) {
+      res.send("Delete message success");
+    } else {
+      res.status(404).json({ error: "Message not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
 const router = express.Router();
 
-router.post("/send-message", authenticateToken, sendMessage);
 router.get("/:senderID/:receiverID", authenticateToken, getAllMessages);
+router.post("/send-message", authenticateToken, sendMessage);
+router.put("/edit-message/:messageID", authenticateToken, editMessage);
+router.delete("/delete-message/:messageID", authenticateToken, deleteMessage);
 
 module.exports = router;
